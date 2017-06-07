be34477c1ca16ab8526dcc56e7e4b76b9c4ebada
Stefan Henneken <stefan.henneken@live.com>
2017-06-07 22:00:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Main");
+            try
+            {
+                Task task1 = Task.Factory.StartNew(Do);
+                task1.Wait();
+            }
+            catch (AggregateException aex)
+            {
+                Console.WriteLine("Exception Main: " + aex.Message);
+                aex.Flatten();
+                foreach (Exception ex in aex.InnerExceptions)
+                    Console.WriteLine("  Exception in Main: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End Main");
+                Console.ReadLine();
+            }
+        }
+        static public void Do()
+        {
+            Console.WriteLine("Start Do");
+            Thread.Sleep(1000);
+            int a = 1;
+            a = a / --a;
+            Console.WriteLine("End Do");
+        }
+    }
+}
